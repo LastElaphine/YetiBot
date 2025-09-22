eda08e39b7e4344e355b7097fb9040c1ed54838d
LastElaphine <shailen.s.patel@gmail.com>
2025-09-21 22:03:46 -0700

    Add a simple bot with slash commands

---
+++ deploy-commands.ts
@@ -0,0 +1,26 @@
+import { REST, Routes } from "discord";
+import { loadCommands } from "./command.ts";
+import config from "./config.json" with { type: "json" };
+
+const commands = await loadCommands();
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(config.token);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.size} application (/) commands.`,
+    );
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      { body: commands.map((value: any) => value.data) },
+    );
+
+    console.log(`Successfully reloaded ${data} application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
