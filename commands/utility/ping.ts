ff8c22c5fd63b19b2c6c4703b7442237ea3d7989
LastElaphine <shailen.s.patel@gmail.com>
2025-09-21 22:03:46 -0700

    Add a simple bot with slash commands

---
+++ commands/utility/ping.ts
@@ -0,0 +1,18 @@
+import { type CommandInteraction, SlashCommandBuilder } from "discord";
+import { Command } from "../../command.ts";
+
+class Ping extends Command {
+	public override get data(): SlashCommandBuilder {
+		return new SlashCommandBuilder()
+			.setName("ping")
+			.setDescription("Replies with Pong!");
+	}
+
+	public override async execute(
+		interaction: CommandInteraction,
+	): Promise<void> {
+		await interaction.reply("Pong!");
+	}
+}
+
+export const command = new Ping();
--- main.ts
+++ main.ts
@@ -1,10 +1,42 @@
-import { Client, Events, GatewayIntentBits } from "discord";
+import { Client, Events, GatewayIntentBits, MessageFlags } from "discord";
+import { type Command, loadCommands } from "./command.ts";
 import config from "./config.json" with { type: "json" };
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const commands = await loadCommands();
+console.log(`Loaded Commands: ${JSON.stringify(commands, null, 2)}`);
+
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = commands.get(interaction.commandName) as Command;
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: "There was an error while executing this command!",
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: "There was an error while executing this command!",
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
 });
 
 client.login(config.token);
