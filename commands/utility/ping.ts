dd1829bcf07eda4f7df315b4923d62cf18113a61
LastElaphine <shailen.s.patel@gmail.com>
2025-09-23 20:34:54 -0700

    Add a give amulet slash command with target user

--- commands/utility/ping.ts
+++ commands/utility/ping.ts
@@ -2,17 +2,17 @@ import { type CommandInteraction, SlashCommandBuilder } from "discord";
 import { Command } from "../../command.ts";
 
 class Ping extends Command {
-	public override get data(): SlashCommandBuilder {
-		return new SlashCommandBuilder()
-			.setName("ping")
-			.setDescription("Replies with Pong!");
-	}
+  public override get data(): SlashCommandBuilder {
+    return new SlashCommandBuilder()
+      .setName("ping")
+      .setDescription("Replies with Pong!");
+  }
 
-	public override async execute(
-		interaction: CommandInteraction,
-	): Promise<void> {
-		await interaction.reply("Pong!");
-	}
+  public override async execute(
+    interaction: CommandInteraction,
+  ): Promise<void> {
+    await interaction.reply("Pong!");
+  }
 }
 
 export const command = new Ping();
--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,5 +1,5 @@
 import { REST, Routes } from "discord";
-import { loadCommands } from "./command.ts";
+import { type Command, loadCommands } from "./command.ts";
 import config from "./config.json" with { type: "json" };
 
 const commands = await loadCommands();
@@ -9,18 +9,18 @@ const rest = new REST().setToken(config.token);
 
 // and deploy your commands!
 (async () => {
-  try {
-    console.log(
-      `Started refreshing ${commands.size} application (/) commands.`,
-    );
+	try {
+		console.log(
+			`Started refreshing ${commands.size} application (/) commands.`,
+		);
 
-    const data = await rest.put(
-      Routes.applicationGuildCommands(config.clientId, config.guildId),
-      { body: commands.map((value: any) => value.data) },
-    );
+		const data = await rest.put(
+			Routes.applicationGuildCommands(config.clientId, config.guildId),
+			{ body: commands.map((value: Command) => value.data) },
+		);
 
-    console.log(`Successfully reloaded ${data} application (/) commands.`);
-  } catch (error) {
-    console.error(error);
-  }
+		console.log(`Successfully reloaded ${data} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
 })();
