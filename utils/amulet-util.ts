cf17800a92e1128e6b6d586458b94e9d8f02b969
LastElaphine <shailen.s.patel@gmail.com>
2025-10-26 14:09:48 -0700

    Add channel helper and update related utilities

--- utils/amulet-util.ts
+++ utils/amulet-util.ts
@@ -1,45 +1,48 @@
 import { setTimeout } from "node:timers";
 import type { User } from "discord";
+import { ChannelHelper } from "./channel-helper.ts";
 
 class AmuletUtil {
-	private static instance: AmuletUtil;
+  private static instance: AmuletUtil;
 
-	private constructor() {}
+  private constructor() {}
 
-	public static getInstance(): AmuletUtil {
-		if (!AmuletUtil.instance) {
-			AmuletUtil.instance = new AmuletUtil();
-		}
-		return AmuletUtil.instance;
-	}
+  public static getInstance(): AmuletUtil {
+    if (!AmuletUtil.instance) {
+      AmuletUtil.instance = new AmuletUtil();
+    }
+    return AmuletUtil.instance;
+  }
 
-	private userId: string | null = null;
-	// private amuletTimeout: NodeJS.Timeout | null = null;
-	private static TIMEOUT_DURATION_MS = 1000;
+  private userId: string | null = null;
+  private channelId: string | null = null;
+  // private amuletTimeout: NodeJS.Timeout | null = null;
+  private static TIMEOUT_DURATION_MS = 1000;
 
-	/*
-	 * Attempts to give a user the amulet.
-	 * @return true if susccessful
-	 */
-	public give(user: User): boolean {
-		if (!this.userId) {
-			this.userId = user.id;
-			//this.amuletTimeout =
-			setTimeout(
-				() => this.clearAmulet(user.id),
-				AmuletUtil.TIMEOUT_DURATION_MS,
-			);
-			return true;
-		}
-		return false;
-	}
+  /*
+   * Attempts to give a user the amulet.
+   * @return true if susccessful
+   */
+  public give(user: User, channelId: string): boolean {
+    if (!this.userId) {
+      this.userId = user.id;
+      //this.amuletTimeout =
+      setTimeout(
+        () => this.clearAmulet(user.id, channelId),
+        AmuletUtil.TIMEOUT_DURATION_MS,
+      );
+      return true;
+    }
+    return false;
+  }
 
-	private clearAmulet(userId: string): void {
-		if (this.userId === userId) {
-			console.log(`clearedAmulet for ${userId}`);
-			this.userId = null;
-		}
-	}
+  private clearAmulet(userId: string, channelId: string): void {
+    if (this.userId === userId) {
+      console.log(`clearedAmulet for ${userId}`);
+      this.userId = null;
+      ChannelHelper;
+    }
+  }
 }
 
 const amuletUtil = AmuletUtil.getInstance();
